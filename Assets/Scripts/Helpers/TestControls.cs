38ae6eb0fd5bfbc13dc87b7037c2d0abc6f0ae37
jortiga <jortiga@gmail.com>
2020-10-06 19:06:11 +0800

    sprite animator

---
+++ Assets/Scripts/Helpers/TestControls.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestControls : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+    }
+}
